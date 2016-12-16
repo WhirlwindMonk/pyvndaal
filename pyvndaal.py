f14418774f0f5bf7fcc2e05f3abc511645f92cc6
Adam Bednarek <whirlwindmonk@gmail.com>
2016-12-16 17:21:02 -0500

    Basic functions for retrieving all vn info by id or first result by name search

---
+++ pyvndaal.py
@@ -0,0 +1,77 @@
+import socket
+import ssl
+from sys import exit
+import json
+
+class VNDAAL:
+	TCP_IP = 'api.vndb.org'
+	TCP_PORT = 19535
+	BUFFER_SIZE = 4096
+	TIMEOUT = 15
+	HOSTNAME = "api.vndb.org"
+	LOGIN_MESSAGE = 'login {"protocol":1,"client":"wwmonktest","clientver":0.1}\u0004'
+
+	def open_connection(self):
+		context = ssl.create_default_context()
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.settimeout(self.TIMEOUT)
+		self.SSL_SOCK = context.wrap_socket(s, server_hostname=self.HOSTNAME)
+		self.SSL_SOCK.connect((self.TCP_IP, self.TCP_PORT))
+		cert = self.SSL_SOCK.getpeercert()
+		if ssl.match_hostname(cert, self.HOSTNAME) != None:
+			return "ERROR: HOSTNAME/CERTIFICATE MISMATCH"
+		return "CONNECTION: SUCCESS"
+		# self.SSL_SOCK.send(self.LOGIN_MESSAGE.en
+	
+	def login(self):
+		self.SSL_SOCK.send(self.LOGIN_MESSAGE.encode())
+		data1 = self.SSL_SOCK.recv(self.BUFFER_SIZE)
+		if data1 != b'ok\x04':
+			return "ERROR: LOGIN FAILED"
+		return "LOGIN: SUCCESS"
+	
+	def _query(self, query):
+		query = query + '\u0004'
+		self.SSL_SOCK.send(query.encode())
+		response = ""
+		while 1:
+			data = self.SSL_SOCK.recv(self.BUFFER_SIZE)
+			try:
+				response += data.decode()
+			except UnicodeDecodeError:
+				print("OS error: {0}".format(err))
+				self.close_connection()
+				return
+			except:
+				print("Unexpected error:", sys.exc_info()[0])
+				self.close_connection()
+				return
+			
+			# print(response)
+			if '\u0004' in response:
+				break
+		if "error" in response:
+			return "ERROR: QUERY INVALID"
+		response = response[8:-1]
+		return json.loads(response)
+	
+	def get_vn_by_id(self, id):
+		query = 'get vn basic,details,anime,relations,tags,stats,screens (id = ' + id + ')'
+		response = self._query(query)
+		return VN(response)
+	
+	def get_vn_by_name(self, name):
+		query = 'get vn basic,details,anime,relations,tags,stats,screens (search ~ "' + name + '")'
+		response = self._query(query)
+		# print(response)
+		return VN(response)
+	
+	def close_connection(self):
+		self.SSL_SOCK.close()
+		
+class VN:
+	data = {}
+	
+	def __init__(self, response):
+		self.data = dict(zip(response['items'][0].keys(), response['items'][0].values()))
